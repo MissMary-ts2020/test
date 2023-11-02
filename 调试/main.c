f9955ffaae92ade03a0aa493c6534509dfe6b6c8
MissMary-ts2020 <hkgjwtl2017@aliyun.com>
2023-11-02 14:00:30 +0800

    14点00分

--- 调试/main.c
+++ 调试/main.c
@@ -2,14 +2,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+//F5 启动调试
+
+int add(int a, int b) {
+
+	return a + b;
+}
 
 int main() {
 	
 	int i = 0;
 	int arr[10] = { 0 };
-	for (; i <= 12; i++) {
+	for (; i < 10; i++) {//设置断点
 		printf("always\n");
-		arr[i] = 0;
+		
+		arr[i] = add(i,tan((double)i));
 	}
 	system("pause");
 	//debug 和 release两个版本的可执行程序编译
